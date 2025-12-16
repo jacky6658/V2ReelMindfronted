55af3688cd38fbed0f691bbb6b1156246c6ceebe
jack <aiagentg888@gmail.com>
2025-12-16 11:34:46 +0800

    Add files via upload

---
+++ client/public/sw.js
@@ -0,0 +1,58 @@
+// Service Worker for ReelMind PWA
+const CACHE_NAME = 'reelmind-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/images/android-chrome-192x192.png',
+  '/assets/images/android-chrome-512x512.png',
+];
+
+// 安装 Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  // 立即激活新的 Service Worker
+  self.skipWaiting();
+});
+
+// 拦截请求，使用缓存
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // 如果缓存中有，返回缓存，否则从网络获取
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// 更新 Service Worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  // 立即控制所有客户端
+  return self.clients.claim();
+});
+
+// 监听来自主线程的消息
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
